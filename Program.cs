60e686e4181286f75b31dcf139f9325c51c674f6
Kelli Stasiak <30395794+kellbell1220@users.noreply.github.com>
2018-03-12 15:17:20 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using static System.Console;
+
+namespace ReceiptApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("\tReceipt Application");
+            //Create an Employee Object
+            Receipt receipt1 = new Receipt();
+            receipt1.receiptNumber = AskForInput("Receipt Number");
+            receipt1.CustomerNumber = AskForInput("Customer's Number");
+            receipt1.firstName = AskForInput("Customer's First Name");
+            receipt1.lastName = AskForInput("Customer's Last Name");
+            receipt1.phoneNumber = AskForInput("Customer's Phone Number");
+            receipt1.address = AskForInput("Customer's Address");
+            receipt1.itemNumber = AskForInput("Item Purchased");
+            receipt1.itemDescription = AskForInput("Item Description");
+            receipt1.ItemPrice = AskItemPrice();
+            receipt1.itemQuantity = AskForInt("Quantity");
+
+            Receipt receipt2 = new Receipt("XYC-2345", "09/25/2013", "Wiper blade", "4XZ23-7", 2, 9.50);
+            Receipt receipt3 = new Receipt("Anna", "Leigh", 21.66);
+            Receipt receipt4 = new Receipt("Brake Fluid", "XQ-1267", 5, 14.78);
+
+            
+            //Printing out First Test
+            WriteLine("\n\tFirst Test");
+            WriteLine(receipt1.ToString());
+
+
+            //Printing out Second Test
+            WriteLine("\n\n\tSecond Test");
+            WriteLine("Receipt Number: " + receipt2.receiptNumber + " - Date: " + receipt2.receiptDate);
+            WriteLine("Description of Part: " + receipt2.itemDescription);
+            WriteLine("Item Number: " + receipt2.itemNumber);
+            WriteLine("Quantity Purchased: " + receipt2.itemQuantity);
+            WriteLine("Total Cost: " + receipt2.ReturnGrandTotal().ToString("C"));
+
+            //Printing out Third Test
+            WriteLine("\n\n\tThird Test");
+            WriteLine(receipt3.ToString());
+
+            //Printing out last test
+            WriteLine("\n\n\tLast Test");
+            WriteLine("Description of Part: " + receipt4.itemDescription);
+            WriteLine("Item Number: " + receipt4.itemNumber);
+            WriteLine("Quantity Purchased: " + receipt4.itemQuantity);
+            WriteLine("Unit Price: " + receipt4.ItemPrice);
+            WriteLine("Total Cost: " + receipt4.ReturnGrandTotal().ToString("C"));
+
+           ReadKey();
+        }
+
+        //Aquiring Customer and Item Information
+        static String AskForInput(String whichOne)
+        {
+            Write("Enter {0}: ", whichOne);
+            String inValue = ReadLine();
+
+            return inValue;
+        }
+
+        //Aquiring Item Price
+        static double AskItemPrice()
+        {
+            Write("Enter Unit Price: ");
+            String inValue = ReadLine();
+
+            return double.Parse(inValue);
+        }
+
+        //Aquiring Item Quantity
+        static int AskForInt(String whichOne)
+        {
+            Write("Enter {0}: ",whichOne);
+            String inValue = ReadLine();
+
+            return int.Parse(inValue);
+        }
+
+    }
+
+}
+   
---
+++ Receipt.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using static System.Console;
+
+
+namespace ReceiptApp
+{
+    class Receipt
+    {
+        //Declaring Variables
+        public String receiptNumber;
+        private String customerNumber;
+        public String firstName;
+        public String lastName;
+        public String phoneNumber;
+        public String address;
+        public String itemNumber;
+        public String itemDescription;
+        private double itemPrice;
+        public int itemQuantity;
+        public String receiptDate;
+
+        //Default Constructor
+
+        public Receipt()
+        {
+
+        }
+
+        public Receipt(String firstName, String lastName, double itemPrice)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.itemPrice = itemPrice;
+        }
+
+        public Receipt(String receiptNumber, String receiptDate, String itemDescription, String itemNumber, int itemQuantity, double itemPrice)
+        {
+            this.receiptNumber = receiptNumber;
+            this.receiptDate = receiptDate;
+            this.itemDescription = itemDescription;
+            this.itemNumber = itemNumber;
+            this.itemQuantity = itemQuantity;
+            this.itemPrice = itemPrice;
+        }
+
+        public Receipt(String itemDescription, String itemNumber, int itemQuantity, double itemPrice)
+        {
+            this.itemDescription = itemDescription;
+            this.itemNumber = itemNumber;
+            this.itemQuantity = itemQuantity;
+            this.itemPrice = itemPrice;
+        }
+
+        //Private Field for Customer Number Getter and Setter
+        public String CustomerNumber
+        {
+            get
+            {
+                return customerNumber;
+            }
+            set
+            {
+                customerNumber = value;
+            }
+
+        }
+
+        //Private Field for ItemPrice Getter and Setter
+        public double ItemPrice
+        {
+            get
+            {
+                return itemPrice;
+            }
+            set
+            {
+                itemPrice = value;
+            }
+
+        }
+
+
+
+        public String ReturnFullName()
+        {
+            String name = firstName + " " + lastName;
+            return name;
+        }
+
+        public double ReturnGrandTotal()
+        {
+            return ItemPrice * itemQuantity;
+        }
+
+
+        public override string ToString()
+        {
+            return "Customer: " + ReturnFullName() + "\nPhone Number: " + phoneNumber + "\nAddress:"+ address + "\nItem Purchased: " + itemNumber 
+                +" Item Description: " + itemDescription + "\nTotal Purchases: " + ReturnGrandTotal().ToString("C");
+        }
+    }
+}
